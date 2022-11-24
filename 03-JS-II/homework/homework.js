ce337cb468f36245bb0d919dd4064c3bad98286f
Juan <juaancho2014@gmail.com>
2022-11-24 20:38:02 -0300

    03-JS-II/homework/homework.js

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -116,8 +116,8 @@ if (num1 === 0 || num2 === 0 || num3 === 0){
   return "Hay negativos";
 }else if (num1 > num2 && num1 > num3){
   return "Número 1 es mayor y positivo";
-}else if (nume3 > num1 && num3 > num2){
-  num3= num3 +1
+}else if (num3 > num1 && num3 > num2){
+  num3 = num3 + 1
   return num3;
 }return false; }
 
