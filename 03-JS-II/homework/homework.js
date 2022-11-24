1b8d831fadaac0a48980ca86aa0569ea4b54174b
Juan <juaancho2014@gmail.com>
2022-11-24 17:47:09 -0300

    03-JS-II/homework/homework.js

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -38,9 +38,9 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
-if (idioma === aleman) {return "Guten Tag!";}
-else if (idioma === mandarin) {return "Ni Hao!"}
-else if (idioma === ingles) {return "Hello!"} else 
+if (idioma === "aleman") {return "Guten Tag!";}
+else if (idioma === "mandarin") {return "Ni Hao!"}
+else if (idioma === "ingles") {return "Hello!"} else 
 return "hola!"
 }
 
