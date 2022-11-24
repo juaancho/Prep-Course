532bd896f4df84c7b356323b6e5f3c87c76454ad
Juan <juaancho2014@gmail.com>
2022-11-24 17:57:14 -0300

    03-JS-II/homework/homework.js

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -67,6 +67,8 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+if (numero === 10 || numero === 5) {return true}
+else {return false}
 }
 
 function estaEnRango(numero) {
