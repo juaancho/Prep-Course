787daf3eb98a4cd611a9c874f487e62e99a4a317
Juan <juaancho2014@gmail.com>
2022-11-25 18:57:00 -0300

    03-JS-II/homework/homework.js

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -156,7 +156,7 @@ return tabla}
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  if (numero.lenth = 3) { return true 
+  if (numero.length = 3) { return true 
   }else return false
 }
 
